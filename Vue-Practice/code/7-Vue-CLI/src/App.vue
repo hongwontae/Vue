874e530e19dc068193e56b67fadd1d9d33a6f766
dnjsxoghd <feelchok1234@gmail.com>
2025-02-05 19:51:38 +0900

    Vue CLI upgrade

---
+++ Vue-Practice/code/7-Vue-CLI/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+          <friend-contact></friend-contact>  
+          <friend-contact></friend-contact>  
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+        data() {
+            return {
+                friends: [
+                    {
+                        id: 'manuel',
+                        name: 'Manuel Lorenz',
+                        number: '010-8489-3121',
+                        email: 'googgo@google.com'
+                    },
+                    {
+                        id: 'HWT',
+                        name: 'Manuel hhh',
+                        number: '010-222-3121',
+                        email: 'goodkdk@google.com'
+                    },
+                ]
+            }
+        },
+
+    }
+</script>
