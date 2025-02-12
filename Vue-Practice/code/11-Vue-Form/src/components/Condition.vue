8708b98e1747152749ef0bac718917de9ed6a1f2
dnjsxoghd <feelchok1234@gmail.com>
2025-02-12 22:26:23 +0900

    Vue Form v-model

---
+++ Vue-Practice/code/11-Vue-Form/src/components/Condition.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <label for="agree">Rate your experience</label>
+        <input 
+            id="agree" 
+            type="text" 
+            :value="modelValue" 
+            @input="updateValue" 
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['modelValue'],  // 부모로부터 v-model 데이터 받음
+    emits: ['update:modelValue'],  // 부모에게 변경된 값 전달
+    methods : {
+        updateValue(e){
+            this.$emit('update:modelValue', e.target.value)
+        }
+    }
+};
+</script>
