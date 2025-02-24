44a869a72c1e1321f313b61a025413346476903f
dnjsxoghd <feelchok1234@gmail.com>
2025-02-24 18:56:35 +0900

    Composition data, methods, computed, watch, ref

--- Vue-Practice/code/17-Vue-CompositionAPI/src/App.vue
+++ Vue-Practice/code/17-Vue-CompositionAPI/src/App.vue
@@ -1,27 +1,66 @@
 <template>
   <section class="container">
-    <h2>{{ userName.name }}</h2>
-    <h2>{{ userName.age }}</h2>
+    <h2>{{ userName }}</h2>
+    <h2>{{ age }}</h2>
+    <button v-on:click="setUser">SetChange</button>
+    <div>
+      <input type="text" placeholder="FirstName" v-on:input="nameHandler" />
+      <input type="text" placeholder="LastName" v-on:input="ageHandler"/>
+      <input type="text" ref="inputRef" v-on:input="testHandler" />
+    </div>
   </section>
 </template>
 
 <script>
-import {ref} from 'vue';
+import {computed, reactive, ref, toRefs, watch} from 'vue';
 export default {
   setup(){
-    const userName = ref({
-      name : 'HWT',
-      age : 30
-    });
+      const user = reactive({
+        age : 28
+      })
+      const firstName = ref('')
+      const lastName = ref('')
 
-    setTimeout(()=>{
-      userName.value.name ='KKKK';
-      userName.value.age = 100
-    }, 2000)
+      const inputRef = ref('')
+      
+      function testHandler(){
+        console.log(inputRef.value.value)
+      }
 
-    return {
-      userName
-    }
+      function setUser(){
+        user.age = 222
+      }
+      const userName = computed(()=>{
+        return firstName.value + ' ' + lastName.value
+      })
+
+      function nameHandler(e){
+        firstName.value = e.target.value;
+      }
+      function ageHandler(e){
+        lastName.value = e.target.value
+      }
+
+      watch(user, (newValue)=>{
+        if(newValue.age == 222){
+          console.log(222 + 'true');
+        }
+      })
+
+      const upgradeUser = toRefs(user)
+
+      return {
+        userName,
+        age : upgradeUser.age,
+        firstName,
+        lastName,
+        setUser,
+        nameHandler,
+        ageHandler,
+        inputRef,
+        testHandler
+      }
+
   }
 };
 </script>
